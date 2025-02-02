60b175b1413ed84b584a94e9eb34b7c234debd46
Revanth-02082006 <revanthsathya.ind@gmail.com>
2025-02-02 11:00:44 +0530

    Create Reverse a String.py

---
+++ 01.02.2025/Reverse a String.py
@@ -0,0 +1,12 @@
+# Function to reverse a string
+def reverse_string(s):
+    return s[::-1]
+
+# Input string
+input_str = input("Enter a string: ")
+
+# Reverse the string
+reversed_str = reverse_string(input_str)
+
+# Print result
+print("Reversed string:", reversed_str)
