8fafbd8efcedac2f6778a5912c3940347eda725f
Revanth-02082006 <revanthsathya.ind@gmail.com>
2025-02-02 10:52:39 +0530

    Create Sum of Two Numbers.py

---
+++ 01.02.2025/Sum of Two Numbers.py
@@ -0,0 +1,13 @@
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Input numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculate sum
+result = add_numbers(num1, num2)
+
+# Print result
+print("The sum is:", result)
